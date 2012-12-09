ef1937198dc690ca24bc1f6e194a78e83d5a3f88
robin <palotai.robin@gmail.com>
2012-12-09 22:53:17 +0100

    modified phantom script to wait for image load

--- html5-test/exec-phantom.js
+++ html5-test/exec-phantom.js
@@ -1,8 +1,22 @@
 // Control file for PhantomJS
 //
 // Renders the webpage and saves its output to a png
+
 var page = require('webpage').create();
+page.settings.localToRemoteUrlAccessEnabled = true
+
+page.onResourceRequested = function(request) {
+    console.log('Request (#' + request.id + '): ' + JSON.stringify(request));
+};
+page.onResourceReceived = function(response) {
+    console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
+};
+
 page.open('index.html', function () {
-    page.render('booted.png');
-    phantom.exit();
+    // Wait a little until the imag is loaded
+    // TODO hook to resource received and apply timeout there?
+    window.setTimeout(function() {
+      page.render('booted.png');
+      phantom.exit();
+    }, 100)
 });
